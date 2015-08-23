77daaafd580d1825aaa3821f1df5891da843c6c8
DataMonky <DataMonky@users.noreply.github.com>
2015-08-24 08:14:48 +1000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -44,4 +44,7 @@ means <- select(merge.df, contains("mean"))
 stds <- select(merge.df, contains("std"))
 merge.df.narrow <- cbind(activity, means, stds)
 
-
+##Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+by_activty <- group_by(merge.df.narrow, activity.name)
+by_activty <- select(by_activty, -(activity.id))
+summarise_each(by_activty, funs(mean))
